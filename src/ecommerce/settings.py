254018f8c97fceb4c02466306389f19ce8f1abfd
shaks <kakhurashvilis@gmail.com>
2023-07-17 16:17:44 +0400

    media file

--- src/ecommerce/settings.py
+++ src/ecommerce/settings.py
@@ -160,15 +160,16 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
-
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
     os.path.join(BASE_DIR, 'admindashboard', 'static'),
 ]
-MEDIA_URL = '/static/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'static/media')
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 
 # HTTPS/SSL Configuration
 # Replace the placeholders with the appropriate paths and settings for your environment
