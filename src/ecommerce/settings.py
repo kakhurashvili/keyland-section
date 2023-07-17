54dcd51ab0e20790384945706852221a1f2f1152
kakhurashvili <95078998+kakhurashvili@users.noreply.github.com>
2023-07-18 01:11:59 +0400

    Update settings.py
    StaticFilesStorage

--- src/ecommerce/settings.py
+++ src/ecommerce/settings.py
@@ -166,7 +166,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
     os.path.join(BASE_DIR, 'admindashboard', 'static'),
 ]
-
+django.contrib.staticfiles.storage.StaticFilesStorage
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
