00a31c4afc3feeeeb8c365407fd5f1355654fe2a
shaks <kakhurashvilis@gmail.com>
2023-07-20 17:58:58 +0400

    db

--- src/ecommerce/settings.py
+++ src/ecommerce/settings.py
@@ -102,16 +102,16 @@ DATABASES = {
 db_from_env = dj_database_url.config(conn_max_age=600)
 DATABASES['default'].update(db_from_env)
 
-""" DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'ecom',
-        'USER': 'postgres',
-        'PASSWORD': 'database',
-        'HOST': 'localhost',
-        'PORT': '5432'
-    }
-} """
+# """ DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'ecom',
+#         'USER': 'postgres',
+#         'PASSWORD': 'database',
+#         'HOST': 'localhost',
+#         'PORT': '5432'
+#     }
+# } """
 
 
 
