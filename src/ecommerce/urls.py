890fdcc712b72a067d3db4cac853c97265060b0c
shaks <kakhurashvilis@gmail.com>
2023-07-20 12:05:51 +0400

    kuber

--- src/ecommerce/urls.py
+++ src/ecommerce/urls.py
@@ -21,7 +21,7 @@ from django.views.static import serve
 from django.urls import re_path
 
 urlpatterns = [
-    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+    # re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
 
     path('dj-admin/', admin.site.urls),
     path('admin/', include('admindashboard.urls')),
